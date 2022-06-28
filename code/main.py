7610f90d78a79bf04e9f82e0f60e0ef1b325d0a9
damian-is <94235867+damian-is@users.noreply.github.com>
2022-06-28 17:59:38 +0200

    basic setup

--- code/main.py
+++ code/main.py
@@ -5,3 +5,24 @@ from settings import *
 class Game:
     def __init__(self):
         pygame.init()
+        self.screen = pygame.display.set_mode(RES)
+        self.clock = pygame.time.Clock()
+
+    def new_game(self):
+        pass
+
+    def update(self):
+        pygame.display.flip()
+        self.clock.tick(FPS)
+        pygame.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+
+    def check_events(self):
+        pass
+    
+    def run(self):
+        while True:
+            self.update()
+            self.draw()

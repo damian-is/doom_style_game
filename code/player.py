565f200879665ca5ad8096b9ccb64908eb591eeb
damian-is <94235867+damian-is@users.noreply.github.com>
2022-06-30 16:57:43 +0200

    Player_set_up

--- code/player.py
+++ code/player.py
@@ -1,1 +1,21 @@
+from settings import *
 import pygame
+import math
+
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGEL
+
+    def movement(self):
+        pass
+
+    def update(self):
+        self.movement()
+
+    @property
+    def pos(self):
+        return self.x, self.y
+        

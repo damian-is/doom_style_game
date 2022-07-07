f85f0811598d86801b0c145a052df618b216684b
damian-is <94235867+damian-is@users.noreply.github.com>
2022-07-07 10:18:04 +0200

    movement

--- code/main.py
+++ code/main.py
@@ -8,6 +8,7 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
+        self.delta_time = 1
         self.new_game()
 
     def new_game(self):
@@ -15,7 +16,7 @@ class Game:
 
     def update(self):
         pygame.display.flip()
-        self.clock.tick(FPS)
+        self.delta_time = self.clock.tick(FPS)
         pygame.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
--- code/player.py
+++ code/player.py
@@ -1,6 +1,7 @@
 from settings import *
 import pygame
 import math
+from main import Game
 
 
 class Player:
@@ -10,7 +11,35 @@ class Player:
         self.angle = PLAYER_ANGEL
 
     def movement(self):
-        pass
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            dx += speed_cos
+            dy += speed_sin
+
+        if keys[pygame.K_s]:
+            dx += -speed_cos
+            dx += -speed_sin
+
+        if keys[pygame.K_a]:
+            dx += speed_sin
+            dy += speed_cos
+
+        if keys[pygame.K_d]:
+            dx += -speed_sin
+            dy += -speed_cos
+
+        self.x += dx
+        self.y += dy
+
+        if keys[pygame.K_LEFT]:
+            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
 
     def update(self):
         self.movement()
@@ -18,4 +47,7 @@ class Player:
     @property
     def pos(self):
         return self.x, self.y
-        
+
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)

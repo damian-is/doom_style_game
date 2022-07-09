897c67764dcd906d32db4a01079c11fac7cdbc09
damian-is <94235867+damian-is@users.noreply.github.com>
2022-07-09 11:18:59 +0200

    raycast_setup

--- code/raycasting.py
+++ code/raycasting.py
@@ -1,1 +1,13 @@
+import pygame
+import math
+from settings import *
 
+class Raycasting:
+    def __init__(self, game):
+        self.game = game
+
+    def ray_cast(self):
+        pass
+
+    def update(self):
+        self.ray_cast()
--- code/settings.py
+++ code/settings.py
@@ -1,3 +1,5 @@
+import math
+
 # Game settings
 RES = WIDHT, HEIGHT = 1600, 900
 FPS = 60
@@ -6,3 +8,11 @@ PLAYER_POS = 1.5, 5 # mini map
 PLAYER_ANGEL = 0
 PLAYER_SPEED = 0.004
 PLAYER_ROT_SPEED = 0.002
+
+# Raycasting settings
+FOV = math.py / 3
+HALF_FOV = FOV/2
+NUM_RAYS = WIDHT // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20

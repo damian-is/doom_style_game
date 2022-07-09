957651c3f6b6f9e0138df51b60a48a41e7362009
damian-is <94235867+damian-is@users.noreply.github.com>
2022-07-09 16:19:31 +0200

    raycasting_fix

--- code/raycasting.py
+++ code/raycasting.py
@@ -15,37 +15,36 @@ class Raycasting:
             sin_a = math.sin(ray_angle)
             cos_a = math.cos(ray_angle)
 
-            # horizontals
-            y_hor, dy = (y_map + 1, 1) if sin_a > 0 else (y_map - 1e6, -1)
-            
+            y_hor, dy = (y_map + 1, 1) if sin_a > 0 else (y_map - 1e-6, -1)
+
             depth_hor = (y_hor - oy) / sin_a
             x_hor = ox + depth_hor * cos_a
 
-            delta_depth = dy /sin_a
+            delta_depth = dy / sin_a
             dx = delta_depth * cos_a
 
             for i in range(MAX_DEPTH):
                 tile_hor = int(x_hor), int(y_hor)
                 if tile_hor in self.game.map.world_map:
                     break
-                x_hor += dx 
+                x_hor += dx
                 y_hor += dy
                 depth_hor += delta_depth
 
             # verticals
-            x_verts, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+            x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
 
-            depth_vert = (x_verts - ox) / cos_a
+            depth_vert = (x_vert - ox) / cos_a
             y_vert = oy + depth_vert * sin_a
 
             delta_depth = dx / cos_a
             dy = delta_depth * sin_a
 
             for i in range(MAX_DEPTH):
-                tile_vert = int(x_verts), int(y_vert)
+                tile_vert = int(x_vert), int(y_vert)
                 if tile_vert in self.game.map.world_map:
                     break
-                x_verts += dx
+                x_vert += dx
                 y_vert += dy
                 depth_vert += delta_depth
 

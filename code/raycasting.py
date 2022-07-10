2b778ad855e95bb57d06751179b8c39af91cc1d6
damian-is <94235867+damian-is@users.noreply.github.com>
2022-07-10 14:12:18 +0200

    texturing_performance_fix

--- code/raycasting.py
+++ code/raycasting.py
@@ -15,14 +15,23 @@ class Raycasting:
         self.object_to_render = []
         for ray, values in enumerate(self.ray_casting_result):
             depth, proj_height, texture, offset = values
+            
+            if proj_height < HEIGHT:
+                wall_column = self.textures[texture].subsurface(
+                    offset * (TEXTURE_SIZE - SCALE), 0, SCALE, TEXTURE_SIZE
+                )
+                wall_column = pygame.transform.scale(wall_column, (SCALE, proj_height))
+                wall_pos = (ray * SCALE, HALF_HEIGHT - proj_height // 2)
+            else:
+                texture_height = TEXTURE_SIZE * HEIGHT / proj_height
+                wall_column = self.textures[texture].subsurface(
+                    offset * (TEXTURE_SIZE - SCALE), HALF_TEXTURE_SIZE - texture_height // 2,
+                    SCALE, texture_height
+                )
+                wall_column = pygame.transform.scale(wall_column, (SCALE, HEIGHT))
+                wall_pos = (ray * SCALE, 0)
 
-            wall_colomn = self.textures[texture].subsurface(
-                offset * (TEXTURE_SIZE - SCALE), 0, SCALE, TEXTURE_SIZE
-            )
-            wall_colomn = pygame.transform.scale(wall_colomn, (SCALE, proj_height))
-            wall_pos = (ray * SCALE, HALF_HEIGHT - proj_height // 2)
-
-            self.object_to_render.append((depth, wall_colomn, wall_pos))
+            self.object_to_render.append((depth, wall_column, wall_pos))
 
     def ray_cast(self):
         self.ray_casting_result = []

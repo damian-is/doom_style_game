57cba4d1f4eeeeca9cd7f6f9b3b23904d86c904c
damian-is <94235867+damian-is@users.noreply.github.com>
2022-07-12 18:55:53 +0200

    finding_the_theta

--- code/sprite_object.py
+++ code/sprite_object.py
@@ -1,1 +1,21 @@
 import pygame
+from settings import *
+import math
+
+class SpriteObject:
+    def __init__(self, game, path = 'resources/sprites/static_sprites/candlebra.png', pos = (10.5, 3.5)):
+        self.game = game
+        self.player = game.player
+        self.x, self.y = pos
+        self.image = pygame.image.load(path).convert_alpha()
+        self.IMAGE_WIDTH = self.image.get_width()
+        self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
+
+    def get_sprite(self):
+        dx = self.x - self.player.x
+        dy = self.y - self.player.y
+        self.dx, self.dy = dx, dy
+        self.theta = math.atan2(dy, dx)
+
+    def update(self):
+        self.get_sprite()

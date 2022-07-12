9b11b8ee73941c1ae4d73d84a5ee974068f022b7
damian-is <94235867+damian-is@users.noreply.github.com>
2022-07-12 18:59:48 +0200

    Removing_the_fishbowl_effect

--- code/sprite_object.py
+++ code/sprite_object.py
@@ -1,3 +1,5 @@
+from re import S
+from numpy import mat
 import pygame
 from settings import *
 import math
@@ -16,6 +18,16 @@ class SpriteObject:
         dy = self.y - self.player.y
         self.dx, self.dy = dx, dy
         self.theta = math.atan2(dy, dx)
+
+        delta = self.theta - self.player.angle
+        if (dx > 0 and self.player.angle > math.pi) or (dx < 0 and dy < 0):
+            delta += math.tau
+
+        delta_rays = delta / DELTA_ANGLE
+        self.screen_x = (HALF_NUM_RAYS + delta_rays) * SCALE
+
+        self.dist = math.hypot(dx, dy)
+        self.norm_dist = self.dist * math.cos(delta)
 
     def update(self):
         self.get_sprite()
